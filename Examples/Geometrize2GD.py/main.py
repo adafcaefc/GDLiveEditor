7b44e2c37d5275f37009e74654a4d4e2f1e45d60
Juan Vincent Nugrahaputra <kjuanvincent@gmail.com>
2020-09-04 15:22:35 +0700

    minor fix

--- Examples/Geometrize2GD.py/main.py
+++ Examples/Geometrize2GD.py/main.py
@@ -11,9 +11,11 @@ from textwrap import indent, dedent
 from gd_object import gd_object_collection
 from gd_pipe import draw_object
 
+from time import sleep
+
 __program_name__ = 'Geometrize2GD.py'
 __author__ = 'Adafcaefc'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 DRAW_SCALE = 1
 CIRCLE_TYPE = [32, 5]
@@ -25,7 +27,7 @@ CHROMA_KEY = [0,0,0]
 OBJECT_LIST = gd_object_collection()
 
 
-def draw_json_to_gd(parsed_json: Dict[str, Any], opaque: bool=True) -> List:
+def draw_json_to_gd(parsed_json: Dict[str, Any], opaque: bool=True) -> None:
 
 	z_order = 0
 
@@ -33,11 +35,11 @@ def draw_json_to_gd(parsed_json: Dict[str, Any], opaque: bool=True) -> List:
 
 		if (shape["type"] in CIRCLE_TYPE) and (shape["score"] > THRESHOLD):
 			if (
-				not (
-					all (
-						shape["color"][i] in range(CHROMA_KEY[i] - TOLERANCE, CHROMA_KEY[i] + TOLERANCE) for i in range(3)
-					)
-				)
+				not all(
+					(
+						-int(TOLERANCE) <= (shape["color"][i] - int(CHROMA_KEY[i])) <= int(TOLERANCE)
+					) for i in range(3)
+				)			
 			) or opaque:
 
 				(x, y, scale, *_), (r, g, b, *_) = shape["data"], shape["color"]
@@ -68,9 +70,10 @@ def main() -> None:
 	print(indent(dedent(
 		f'''
 		{Fore.CYAN}{__program_name__} v{__version__} by {__author__}
-		{Fore.MAGENTA}Have fun!
 
-		{Fore.RED}NOT FOR PUBLIC USE!'''
+		{Fore.MAGENTA}Have fun, happy creating!
+
+		{Fore.RED}Make sure that you have GD Live Editor installed!'''
 	), " "))
 
 	while True:
@@ -79,16 +82,29 @@ def main() -> None:
 
 		print(indent(dedent(
 			f'''
-			{Fore.YELLOW}Enter your geometrize JSON file name{Fore.GREEN}
+			{Fore.YELLOW}Enter your geometrize JSON file name.{Fore.GREEN}
 			'''
 		), " "))
-		
-		json_path = input(" ")
 
-		try:
-			json_file = open(json_path, 'r').read()
-		except:
-			json_file = open(json_path + '.json', 'r').read()
+		json_file = False
+		while not json_file:
+			json_path = input(" ")
+			try:
+				json_file = open(json_path, 'r').read()
+			except:
+				try:
+					json_file = open(json_path + '.json', 'r').read()
+				except:
+					clear_line(2)
+					print(indent(dedent(
+						f'''
+						{Fore.RED}File not found!{Fore.GREEN}
+						'''
+					), " "))
+					sleep(1)
+					clear_line(2)
+
+
 
 		parsed_json = json.loads(json_file)
 
@@ -96,7 +112,7 @@ def main() -> None:
 
 		print(indent(dedent(
 			f'''
-			{Fore.YELLOW}Enter image scale{Fore.GREEN}
+			{Fore.YELLOW}Enter image scale.{Fore.GREEN}
 			'''
 		), " "))
 
@@ -104,12 +120,45 @@ def main() -> None:
 
 		clear_line(4)
 
-		draw_json_to_gd(parsed_json)
+		print(indent(dedent(
+			f'''
+			{Fore.YELLOW}Enter chroma key filter in {Fore.GREEN}r,g,b{Fore.YELLOW} format, fill 0 to use the opaque mode.{Fore.GREEN}
+			'''
+		), " "))
+
+		chroma_key_input = input(" ").split(',')
+
+		clear_line(4)
+
+		is_opaque = not (len(chroma_key_input) == 3)
+
+		if not is_opaque:
+
+			CHROMA_KEY = chroma_key_input
+
+			print(indent(dedent(
+				f'''
+				{Fore.YELLOW}Enter chroma key strength between {Fore.GREEN}0, 255{Fore.GREEN}
+				'''
+			), " "))
+
+			TOLERANCE = input(" ")
+
+			clear_line(4)
 
 
-def clear_line(num):
+		
+		draw_json_to_gd(parsed_json, is_opaque)
 
-  print((Cursor.UP() + colorama.ansi.clear_line()) * num, end='')
+
+		
+
+		
+
+
+def clear_line(number_of_lines: int) -> None:
+
+  print((Cursor.UP() + colorama.ansi.clear_line()) * number_of_lines, end='')
 
 
 if __name__ == '__main__':
